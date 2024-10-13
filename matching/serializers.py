4c440e4dd11612b59b1904b7abd7357e5e61ec02
riyanalla <riyathenalla@gmail.com>
2024-10-13 13:43:56 -0700

    Adding serializers.py and settings.py

--- matching/serializers.py
+++ matching/serializers.py
@@ -1,1 +1,7 @@
 from rest_framework import serializers
+from .models import Pair
+
+class PairSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pair
+        fields = '__all__'  
